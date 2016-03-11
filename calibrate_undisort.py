4a0148f139c97fb8d558e84a838a8aaa86d206e5
Lars Brusletto <larsbrusletto@gmail.com>
2016-03-11 12:54:02 +0100

    added the calibrate_undistort, and disparityMap

---
+++ calibrate_undisort.py
@@ -0,0 +1,317 @@
+# To run from terminal
+# python calibrate_undisort.py
+
+import cv2
+import sys
+import numpy as np
+import os
+import imutils
+# -------------- SET INFORMATION TO BE USED --------
+#Import Information
+#filename = 'GOPR005.MP4'
+
+def resizeImages(self,in_width):
+    for i, img in enumerate(self.images):
+        self.images[i] = imutils.resize(self.images[i], width=in_width)
+
+# --> This method does clahe on lab space to keep the color
+# transform to lab color space and conduct clahe filtering on l channel then merge and transform back
+def claheAdjustImages(img):
+    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe = cv2.createCLAHE(clipLimit=6.0,tileGridSize=(8, 8))
+    #self.logger.info("The clahe type {}".format(type(clahe.type)))
+    print 'color adjusting image'
+    lab_image = cv2.cvtColor(img, cv2.cv.CV_RGB2Lab)
+    print 'converted image to Lab space'
+    lab_planes = cv2.split(lab_image)
+    lab_planes[0] = clahe.apply(lab_planes[0])
+    print 'apply clahe to image channel 0 --> L sapce'
+    # Merge the the color planes back into an Lab image
+    lab_image = cv2.merge(lab_planes, lab_planes[0])
+    print 'merge channels back and transform back to rgb space'
+    return cv2.cvtColor(lab_image, cv2.cv.CV_Lab2RGB)
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+def fixCalibrationOriginal(folderpath_calib_images, newFolderName):
+    for filename in os.listdir(folderpath_calib_images):
+        img = cv2.imread(os.path.join(folderpath_calib_images, filename))
+        if img is not None:
+            # 1 run clahe on the image
+            img = claheAdjustImages(img)
+
+
+
+            # 2 convert to grayscale
+            grey_image = cv2.cvtColor(img, cv2.cv.CV_RGB2GRAY)
+
+            # apply treshold
+            # Set threshold and maxValue
+            thresh=70
+            maxValue=255
+
+            # Threshold to Zero ( type = THRESH_TOZERO )
+            #th, THRESH_TOZERO_img = cv2.threshold(grey_image, thresh, maxValue, cv2.THRESH_TOZERO)
+
+            #th, THRESH_TOZERO_INV_img = cv2.threshold(grey_image, thresh, maxValue, cv2.THRESH_BINARY_INV)
+            #th, THRESH_TOZERO_INV_img = cv2.threshold(THRESH_TOZERO_INV_img, thresh, maxValue, cv2.THRESH_BINARY_INV)
+            th, dst = cv2.threshold(grey_image, thresh, maxValue, cv2.THRESH_BINARY_INV)
+
+
+            # addWeighted
+            blured_image = cv2.GaussianBlur(dst, (5, 5), 105) # //hardcoded filter size, to be tested on 50 mm lens
+            wightedGray = cv2.addWeighted(src1=dst, alpha=0.8, src2=grey_image, beta=-0.8, gamma=0.1)
+
+            # # TODO: try resize the image  resize()
+
+            # 3 save image to folder
+            # images.append(img)
+            cv2.imwrite(os.path.join(newFolderName, filename), wightedGray)
+
+# Finally the function will go through the calbration images and display the undistorted image.
+def ImageProcessing(folderpath_calib_images, board_w, board_h, board_dim, image_size):
+    # 0 get images
+    #folderpath_images = r"C:\MASTER_DATASET\Desembertokt\Mosaic Camera\Calibration Pictures\test_calib_images" # r is there becouse of --> http://stackoverflow.com/questions/7268618/python-issues-with-directories-that-have-special-characters
+    #images_list = os.listdir(folderpath_images)
+
+    images_list = load_images_from_folder(folderpath_calib_images)
+
+    nr_of_pics = len(images_list)
+    n_boards =  nr_of_pics #2
+
+    # 1 ----- Initializing variables
+    board_n = board_w * board_h
+    objectPoints = []
+    imagePoints = []
+
+    npts = np.zeros((n_boards, 1), np.int32)
+    intrinsic_matrix = np.zeros((3, 3), np.float32)
+    distCoeffs = np.zeros((5, 1), np.float32)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+
+    # 2 ---- prepare object points based on the actual dimensions of the calibration board
+    # like (0,0,0), (25,0,0), (50,0,0) ....,(200,125,0)
+    # preallocate
+    object_points = np.zeros((board_h*board_w, 3), np.float32)
+    # fill
+    object_points[:, :2] = np.mgrid[0:(board_w*board_dim):board_dim, 0:(board_h*board_dim):board_dim].T.reshape(-1, 2)
+
+
+    # 3 Loop through the images.  Find checkerboard corners and save the data to imagePoints.
+    for i in range(1, n_boards): #range(1, n_boards + 1):
+
+        #Loading images
+        print 'Loading... Calibration_Image' # + str(i) + '.jpg'  #'.png'
+        # image = cv2.imread('Calibration_Image' + str(i) + '.jpg' )  # '.png')
+        #image = cv2.imread(images_list[i])
+        image = images_list[i]
+
+        print 'image = images_list[i]'
+
+
+
+
+        # ---> Find chessboard corners
+        # needs checkerboard with white bacground arounf them --> http://stackoverflow.com/questions/17993522/opencv-findchessboardcorners-function-is-failing-in-a-apparently-simple-scenar/20187143#20187143
+        [found, corners] = cv2.findChessboardCorners(image=image, patternSize=(board_w, board_h), corners=board_w*board_h,
+                                                     flags=cv2.cv.CV_CALIB_CB_ADAPTIVE_THRESH + cv2.cv.CV_CALIB_CB_FILTER_QUADS + cv2.cv.CV_CALIB_CB_NORMALIZE_IMAGE)
+                                                     #cv2.cv.CV_CALIB_CB_ADAPTIVE_THRESH) #+ cv2.cv.CV_CALIB_CB_NORMALIZE_IMAGE)
+
+
+        # bool found = findChessboardCorners(image, board_sz, corners, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS)
+
+        if found:
+            print (found) # TODO: it says findChessboardCorners has not found the chessboard corners
+            print ('found chessboard corners')
+        else:
+            print found
+            print "did not find chessboard in image" + str(i)
+
+        if found == True:
+
+            #Add the "true" checkerboard corners
+            objectPoints.append(objp)
+            # len(objectPoints0 = 0 --> it finds no checkerboard... --> found == FALSE
+
+            #Improve the accuracy of the checkerboard corners found in the image and save them to the imagePoints variable.
+            cv2.cornerSubPix(image, corners, (20, 20), (-1, -1), criteria)
+            imagePoints.append(corners)
+
+            #Draw chessboard corners
+            cv2.drawChessboardCorners(image, (board_w, board_h), corners, found)
+
+            #Show the image with the chessboard corners overlaid.
+            cv2.imshow("Corners", image)
+            cv2.waitKey(0)
+
+        char = cv2.waitKey(0)
+
+    cv2.destroyWindow("Corners")
+
+    print ''
+    print 'Finished processes images.'
+
+    # 4 Calibrate the camera
+    print 'Running Calibrations...'
+    print(' ')
+
+    # calibrateCamera(pointsmm,points,Size(640,480),cameraMatrix, distCoeffs, rvec, tvec );
+    if found == True:
+        print ('len(objectPoints) : ' + str(len(objectPoints)))
+        print ('objectPoints : ' + objectPoints[0])
+        print ('imagePoints : ' + imagePoints)
+        print ('image_size : ' + image_size)
+
+        [ret, intrinsic_matrix, distCoeff, rvecs, tvecs] = cv2.calibrateCamera(objectPoints, imagePoints, image_size, None, None) # image.shape[::-1], None, None)
+
+        # 5 Save matrices
+        print('Intrinsic Matrix: ')
+        print(str(intrinsic_matrix))
+        print(' ')
+        print('Distortion Coefficients: ')
+        print(str(distCoeff))
+        print(' ')
+
+        # 6 Save data
+        print 'Saving data file...'
+        np.savez('calibration_data', distCoeff=distCoeff, intrinsic_matrix=intrinsic_matrix)
+        print 'Calibration complete'
+
+        # 7 Calculate the total reprojection error.  The closer to zero the better.
+        tot_error = 0
+        for i in xrange(len()): # maybe n_boards
+            imgpoints2, _ = cv2.projectPoints([i], rvecs[i], tvecs[i], intrinsic_matrix, distCoeff)
+            error = cv2.norm(imagePoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+            tot_error += error
+
+        print "total reprojection error: ", tot_error/len()
+
+        # return variables to be used by UndistortImages
+        return intrinsic_matrix, distCoeff
+
+    else:
+        print found
+        print "did not find chessboard in image" + str(i)
+
+    '''
+    # ---->  8 Undistort Images and display them
+    for i in range(1, n_boards + 1):
+
+        #Loading images
+        print 'Loading... Calibration_Image' + str(i) + '.png'
+        image = cv2.imread(images_list(i))
+        # image = cv2.imread('Calibration_Image' + str(i) + '.png')
+
+        # undistort
+        undistorted_img = cv2.undistort(image, intrinsic_matrix, distCoeff, None)
+
+        cv2.imshow('Undisorted Image', undistorted_img)
+
+        char = cv2.waitKey(0)
+    '''
+    cv2.destroyAllWindows()
+
+
+
+def UndistortImages(folderpath_undistort_theese_images, intrinsic_matrix, distCoeff):
+      # ---->  8 Undistort Images and display them
+
+    images_list = load_images_from_folder(folderpath_undistort_theese_images)
+    nr_of_pics = len(images_list)
+
+    for i in range(1, nr_of_pics + 1):
+        # 1 Loading images
+        print 'Loading... Calibration_Image' + str(i) + '.png'
+        image = cv2.imread(images_list(i))
+
+        # 2 Undistort the Image
+        undistorted_img = cv2.undistort(image, intrinsic_matrix, distCoeff, None)
+
+        # 3 show the undistorted image
+        cv2.imshow('Undisorted Image', undistorted_img)
+        cv2.waitKey(0)
+
+        # 4 save the undistorted image
+
+
+    cv2.destroyAllWindows()
+
+
+
+# The program is run from here ###################################################
+
+
+#print('We will collect ' + str(n_boards) + ' calibration images.')
+
+#ImageCollect(filename, n_boards)
+
+
+
+
+#folderpath_images = r"C:\MASTER_DATASET\Desembertokt\Mosaic Camera\Calibration Pictures\test_calib_images" # r is there becouse of --> http://stackoverflow.com/questions/7268618/python-issues-with-directories-that-have-special-characters
+
+folderpath_undistort_theese_images = "somewhere"
+
+#images_list = os.listdir(folderpath_images)
+#nr_of_pics = len(images_list)
+
+#Input the number of board images to use for calibration (recommended: ~20)
+# n_boards =  nr_of_pics #2
+#Input the number of squares on the board (width and height)
+board_w = 19 # 9
+board_h = 15 # 6
+#Board dimensions (typically in cm)
+board_dim = 80
+#Image resolution
+image_size = (4008, 2672) # TODO: get the image size directly from the image
+# stereo rig : (1360, 1024)
+# mono rig = (4008, 2672)
+# gopro rig = (1920, 1080)
+
+# runs the method from here
+
+# since the calibration board does not have a white boarder arouynd them
+# first "FIX" the calibration images
+
+# save them to newFolderName
+newFolderName = "fixed_Calibration_Images"
+folderpath_calib_images = r"calibrationIMG"
+
+
+print("Starting camera calibration....")
+print("Step 1: Image fixing")
+print("calibration images.")
+print(" ")
+if not os.path.isdir(newFolderName):
+    os.mkdir(newFolderName)
+    fixCalibrationOriginal(folderpath_calib_images, newFolderName)
+else:
+    fixCalibrationOriginal(folderpath_calib_images, newFolderName)
+# they are saved as:
+
+print(' ')
+print('All the calibration images are fixed.')
+print('------------------------------------------------------------------------')
+
+
+# get the calibration parameters
+# from images in folder
+print('Step 2: Calibration of fixed images')
+print('We will analyze the images and calibrate the camera.')
+print(' ')
+folderpath_calib_images = r"fixed_Calibration_Images"
+[intrinsic_matrix, distCoeff] = ImageProcessing(folderpath_calib_images, board_w, board_h, board_dim, image_size)
+
+# undistort images using calibration data : intrinsic_matrix & distCoeff
+print('Step 3:  and rectify the original images')
+print('we use the intrinsic_matrix & distCoeff to undistort the images.')
+print(' ')
+#UndistortImages(folderpath_undistort_theese_images, intrinsic_matrix, distCoeff)
+
+# rectify images
---
+++ disparityMap.py
@@ -0,0 +1,138 @@
+
+# python disparityMap.py
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# ---->  Next we find the epilines. Epilines corresponding to the points in first image is drawn on second image.
+# So mentioning of correct images are important here.
+#  We get an array of lines. So we define a new function to draw these lines on the images.
+
+def drawlines(img1, img2, lines, pts1, pts2):
+    ''' img1 - image on which we draw the epilines for the points in img2
+    lines - corresponding epilines '''
+    [r, c] = img1.shape
+    img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+        color = tuple(np.random.randint(0,255,3).tolist()) # create a random color
+        [x0, y0] = map(int, [0, -r[2]/r[1] ])
+        [x1, y1] = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+        img1 = cv2.line(img1, (x0, y0), (x1,y1), color, 1)
+
+        img1 = cv2.circle(img1, tuple(pt1), 5, color, -1)
+        img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
+        return img1, img2
+
+
+# So first we need to find as many possible matches between two images to find the fundamental matrix. For this, we use SIFT descriptors with FLANN based matcher and ratio test.
+
+
+# WARNING: program needs to run on rectified images
+
+#img1 = cv2.imread('calibrationIMG/calibration_image1.jpg', 0)  #queryimage # left image
+#img2 = cv2.imread('calibrationIMG/calibration_image2.jpg', 0) #trainimage # right image
+
+img1 = cv2.imread('images/calib_images/left01.jpg', 0)
+img2 = cv2.imread('images/calib_images/right01.jpg', 0)
+
+
+# find the keypoints and descriptors with SIFT
+sift = cv2.SIFT()
+kp1, des1 = sift.detectAndCompute(img1, None)
+kp2, des2 = sift.detectAndCompute(img2, None)
+
+# FLANN parameters
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+search_params = dict(checks=50)
+
+flann = cv2.FlannBasedMatcher(index_params,search_params)
+matches = flann.knnMatch(des1, des2, k=2)
+
+good = []
+pts1 = []
+pts2 = []
+
+# --->  Get the best matches from both the images.
+# ratio test as per Lowe's paper
+for i, (m, n) in enumerate(matches):
+    if m.distance < 0.8*n.distance:
+        good.append(m)
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+# Now we have the list of best matches from both the images.
+
+
+
+
+# pts1 = np.int32(pts1)
+# pts2 = np.int32(pts2)
+
+pts1 = np.float32(pts1) # float32(pts1)
+pts2 = np.float32(pts2) # float32(pts2)
+print(pts1 )
+#print('pts2 : ' + pts2 )
+
+# --> Let's find the Fundamental Matrix.
+[F, mask] = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+
+
+# We select only inlier points
+pts1 = pts1[mask.ravel()==1]
+pts2 = pts2[mask.ravel()==1]
+
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+
+# Rectify undistoreted
+[height, width] = tuple(img1.shape[1::-1])
+imageSize = (height, width)
+rectification_homography1, rectification_homography2 = cv2.cv.StereoRectifyUncalibrated(pts1, pts2, F, imageSize,  threshold=5)
+
+
+# warpperspective
+warpImage1 = cv2.warpPerspective(img1, rectification_homography1, imageSize)
+warpImage2 = cv2.warpPerspective(img2, rectification_homography2, imageSize);
+
+
+
+# ------> Now we find the epilines in both the images and draw them.
+
+ # Find epilines corresponding to points in right image (second image) and
+ # drawing its lines on left image
+lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F)
+#  The format of point matrix is unsupported in function cvComputeCorrespondEpilines
+# --> needs to run program on rectified images
+lines1 = lines1.reshape(-1,3)
+[img5, img6] = drawlines(img1, img2, lines1, pts1, pts2)
+
+# Find epilines corresponding to points in left image (first image) and
+# drawing its lines on right image
+lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
+lines2 = lines2.reshape(-1,3)
+[img3, img4] = drawlines(img2, img1, lines2, pts2, pts1)
+
+plt.subplot(121), plt.imshow(img5)
+plt.subplot(122), plt.imshow(img3)
+plt.show()
+
+'''
+Exercises
+
+One important topic is the forward movement of camera. Then epipoles will be seen at the same locations in both with epilines emerging from a fixed point. See this discussion.
+Fundamental Matrix estimation is sensitive to quality of matches, outliers etc. It becomes worse when all selected matches lie on the same plane. Check this discussion.
+'''
+
+
+'''
+imgL = cv2.imread('calibrationIMG/calibration_image1.jpg', 0)
+imgR = cv2.imread('calibrationIMG/calibration_image2.jpg', 0)
+
+# Create disparity images
+stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL, imgR)
+plt.imshow(disparity, 'gray')
+plt.show()
+'''
